54b69a1ce92b827b27c1acca1dfe6ba7fd108044
Yury <53217369+yurichernyshov@users.noreply.github.com>
2020-06-15 15:13:29 +0500

    Add files via upload

---
+++ simbank_start.sh
@@ -0,0 +1,48 @@
+﻿#!/bin/sh
+
+git clone https://github.com/yurichernyshov/simbank.git
+
+cd ./simbank/project
+
+docker-compose build
+
+docker-compose up -d
+
+status=0
+
+while [ 1 ]
+do
+echo "---------------------------------------------------"
+echo "|                                                 |"
+echo "| simbank application is started                  |"
+echo "| options:                                        |"
+echo "| 1): createsuperuser                             |"
+echo "| 2): start simbank                               |"
+echo "| 3): stop simbank                                |"
+echo "| 4): status                                      |"
+echo "| x): exit                                        |"
+echo "|                                                 |"
+echo "---------------------------------------------------"
+
+read status
+
+case $status in
+ "1" )
+   docker-compose exec web python manage.py createsuperuser
+ ;;
+ "2" )
+   docker-compose start
+ ;;
+ "3" )
+   docker-compose stop
+ ;;
+ "4" )
+   docker info
+ ;;
+ "x" )
+   docker-compose stop
+   exit 0
+ ;;
+esac
+
+done
